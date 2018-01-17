da298e975f8233e67ff89e30f677d60dbcf99a2e
Nikola07 <paul-jfg@kabelmail.de>
2018-01-17 18:09:23 +0100

    changed attribute name

--- src/execution_modes/TrainingMode.py
+++ src/execution_modes/TrainingMode.py
@@ -9,8 +9,8 @@ class TrainingMode:
 
     @staticmethod
     def execute_training(self, path_to_training_files, path_to_stopword_file, path_to_indicator_file, training_data_name,
-                         article_length,path_to_vector_file):
-        articles = ArticleReader.read_articles(path_to_training_files, article_length)
+                         minimal_article_length, path_to_vector_file):
+        articles = ArticleReader.read_articles(path_to_training_files, minimal_article_length)
 
         articles = StopwordRemover.remove_stopwords_from_corpus(StopwordFileReader.read_stopwordfile(path_to_stopword_file), articles)
 
