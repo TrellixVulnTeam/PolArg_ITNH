a047dcb3b041f721dd8e84da8baa379dd610811e
Nikola07 <paul-jfg@kabelmail.de>
2018-01-17 07:51:02 +0100

    implemented tokencounter

--- src/argumentation_parsing/TokenCounter.py
+++ src/argumentation_parsing/TokenCounter.py
@@ -1,6 +1,15 @@
 class TokenCounter:
 
-    def countTokens(self, sentenceToCount):
-        tokenCount = len(sentenceToCount)
+    def countTokens(self, sentence_to_count):
+        # only needed if sentence_to_count is a string
+        token_list = ' '.join(self, sentence_to_count)
+        token_count = len(token_list)
 
-        return tokenCount
+        return token_count
+
+    def count_Article_Token(self, article_to_count):
+        # only needed if article_to_count is a string
+        token_list = ' '.join(self, article_to_count)
+        token_count = len(token_list)
+
+        return token_count
