214e096f7bf4b076dc43c3c48c20be4e07ece1d7
Nikola07 <paul-jfg@kabelmail.de>
2018-01-18 15:59:23 +0100

    implemented count methods

--- src/argumentation_parsing/TokenCounter.py
+++ src/argumentation_parsing/TokenCounter.py
@@ -1,15 +1,42 @@
 class TokenCounter:
 
-    def count_tokens(self, sentence_to_count):
-        # only needed if sentence_to_count is a string
-        token_list = ' '.join(self, sentence_to_count)
-        token_count = len(token_list)
-
-        return token_count
-
-    def count_article_token(self, article_to_count):
+    @staticmethod
+    def count_article_tokens(self, article_to_count):
         # only needed if article_to_count is a string
-        token_list = ' '.join(self, article_to_count)
+        token_list = article_to_count.split(' ')
         token_count = len(token_list)
 
         return token_count
+
+    def count_average_sentence_length(self, article_to_count):
+        # only needed if article_to_count is a string
+        sentence_list = article_to_count.split('. ')
+        # counts the number of sentences
+        number_of_sentences = len(sentence_list)
+        # adds the sentence lengths
+        word_counter = 0
+        for sentence in sentence_list:
+            word_list = ' '.join(self, sentence)
+            sentence_length = len(word_list)
+            word_counter += sentence_length
+
+        average_sentence_length = word_counter / number_of_sentences
+
+        return average_sentence_length
+
+    def count_number_of_sub_sentences(self, article_to_count):
+        token_list = ' '.join(self, article_to_count)
+        token_list = token_list.split(',')
+        sub_sentence_list = []
+        for sentence in token_list:
+            if '. ' in sentence:
+                continue;
+            else:
+                if len(sentence) > 25:
+                    sub_sentence_list.append(sentence)
+        return len(sub_sentence_list)
+
+
+
+
+
