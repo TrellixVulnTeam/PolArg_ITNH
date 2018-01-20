688d1f5cfb3433d60c774ba1038a6627bbb6f295
Christoph Maier <christophmaier@Christophs-MBP.fritz.box>
2018-01-20 12:05:29 +0100

    adds vector serialization

--- src/vector/VectorFileHandler.py
+++ src/vector/VectorFileHandler.py
@@ -1,14 +1,17 @@
-import ArgumentVector
-
+import pickle
 
 class VectorFileHandler:
 
     @staticmethod
-    def read_vector_from_file(self, path_to_vector_file):
-        vector_from_file = ArgumentVector.__init__()
+    def read_vector_from_file(path_to_vector_file):
+        pickle_in = open(path_to_vector_file,"rb")
+        vector_from_file = pickle.load(pickle_in)
 
         return vector_from_file
 
     @staticmethod
-    def safe_vector_in_file(self, path_to_vector_file):
+    def safe_vector_in_file(vector, path_to_vector_file):
+        pickle_out = open(path_to_vector_file,"wb")
+        pickle.dump(vector,pickle_out)
+        pickle_out.close()
         print("Vector safed in File!")
