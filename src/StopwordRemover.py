2d74cb3aa5512407dfd0c68ddaf888b5dce9e660
Fazeplant <bumes.michael@googlemail.com>
2018-01-22 19:15:08 +0100

    Implemented StopwordRemover

--- src/StopwordRemover.py
+++ src/StopwordRemover.py
@@ -1,21 +1,30 @@
+import codecs
+from nltk.corpus import stopwords
+
 class StopwordRemover(object):
+    def remove_stopwords_from_article(self, article):
+        stop_words = set(stopwords.words('german'))
+        removed_stopword_list = [i for i in article._content.split() if i not in stop_words]
+        removed_stopword_string = ' '.join(removed_stopword_list)
+        stopword_to_remaining_words_ratio = len(removed_stopword_string)/len(article.content)
+        article.content = removed_stopword_string
 
-    @staticmethod
-    def remove_stopwords_from_sentence(self, stopwordlist, sentence):
-        print("Iterate over sentence and compare tokens with the stopwordlist. "
-              "Remove token if it occurs in the stopwordlist")
-
-        return sentence
-
-    def remove_stopwords_from_article(self, stopwordlist, article):
-        modified_article = list()
-        for sentence in article:
-            modified_article.append(self.remove_stopwords_from_sentence(stopwordlist, sentence))
-        return modified_article
-
-    @staticmethod
-    def remove_stopwords_from_corpus(self, stopwordlist, corpus):
-        modified_corpus = list()
-        for article in corpus:
-            modified_corpus.append(self.remove_stopwords_from_article(stopwordlist, article))
-        return modified_corpus
+    # @staticmethod
+    # def remove_stopwords_from_sentence(self, stopwordlist, sentence):
+    #     print("Iterate over sentence and compare tokens with the stopwordlist. "
+    #           "Remove token if it occurs in the stopwordlist")
+    #
+    #     return sentence
+    #
+    # def remove_stopwords_from_article(self, stopwordlist, article):
+    #     modified_article = list()
+    #     for sentence in article:
+    #         modified_article.append(self.remove_stopwords_from_sentence(stopwordlist, sentence))
+    #     return modified_article
+    #
+    # @staticmethod
+    # def remove_stopwords_from_corpus(self, stopwordlist, corpus):
+    #     modified_corpus = list()
+    #     for article in corpus:
+    #         modified_corpus.append(self.remove_stopwords_from_article(stopwordlist, article))
+    #     return modified_corpus
