a2c9e40ece2dbb1feaaa22639a872f98f45814a8
Nikola07 <paul-jfg@kabelmail.de>
2018-01-23 16:27:02 +0100

    build in ElementTree

--- src/ArticleReader.py
+++ src/ArticleReader.py
@@ -2,6 +2,7 @@
 from glob import glob
 from nltk.corpus import XMLCorpusReader
 from nltk.corpus import PlaintextCorpusReader
+import xml.etree.ElementTree as ET
 
 
 #Anmerkung C: Pfade beim Methodenaufruf übergeben. Evtl sinnvoll die Artikel in Sätze zu splitten und eine Liste
@@ -25,7 +26,7 @@ class ArticleReader():
 
         reader_online = XMLCorpusReader(loc_corpus, years_online)
         reader_magazine = XMLCorpusReader(loc_corpus, years_magazine)
-        reader_corpus = PlaintextCorpusReader(loc_corpus + '/Magazin/Corpus-Magazin', '.*', encoding='utf-16')
+        # reader_corpus = PlaintextCorpusReader(loc_corpus + '/Magazin/Corpus-Magazin', '.*', encoding='utf-16')
 
         articles = []
         """
@@ -41,26 +42,43 @@ class ArticleReader():
             print(len(article_list))
         """
         # an example for the magazine corpus
+        i = 1
         for fileid in reader_magazine.fileids():
-            words = reader_magazine.words(fileid)
-            string = ' '.join(words)
-            article_list = string.split('SP DER SPIEGEL')
-            for article in article_list:
-                if len(article.split(' ')) > article_length:
-                    add_article = Article()
-                    add_article.content = article
-                    articles.append(add_article)
-            print(len(article_list))
+            tree = ET.ElementTree(file=fileid)
+            for elem in tree.iter(tag='artikel'):
+                add_article = Article()
+                i += 1
+                for child in elem.iter(tag='metadaten'):
+                    for id in child.iter(tag='artikel-id'):
+                        add_article.id = id
+                for child in elem.iter(tag='inhalt'):
+                    if child.tag is None:
+                        break
+                    for baby in child.iter(tag='text'):
+                        if baby.tag is None:
+                            break
+                        article_text = ""
+                        for text in baby.iter(tag='absatz'):
+                            if text.text is None:
+                                break
+                            if text is not None:
+                                article_text += text.text
+
+                            # print(len(string))
+                        if len(article_text) > article_length:
+                            add_article.content = article_text
+                articles.append(add_article)
         """
         for fileid in reader_corpus.fileids():
             if len(reader_corpus.raw(fileid)) > article_length:
                 articles.append(reader_corpus.raw(fileid))
                 print(len(reader_corpus.raw(fileid)))
         """
-        politic = [article for article in articles if article.content.__contains__('Politik')]
+        # politic = [article for article in articles if article.content.__contains__('Politik')]
         print(len(articles))
-        print(len(politic))
-        return politic
+        # print(len(politic))
+        # return politic
+        return articles
 
     def read_articles_learn(loc_corpus, article_length):
 
