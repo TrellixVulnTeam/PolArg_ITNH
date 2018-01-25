168abfaa96a96216c7ee126c9896a205083428c6
Nikola07 <paul-jfg@kabelmail.de>
2018-01-25 15:37:46 +0100

    added title

--- src/Article.py
+++ src/Article.py
@@ -11,6 +11,7 @@ class Article(object):
         self._tagged_content = None
         self._contains_argumentation = False
         self._keywords = None
+        self._title = None
 
     @property
     def id(self):
@@ -59,3 +60,11 @@ class Article(object):
     @keywords.setter
     def keywords(self, val):
         self._keywords = val
+
+    @property
+    def title(self):
+        return self._content
+
+    @title.setter
+    def title(self, val):
+        self._title = val
