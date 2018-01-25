459f8641e26c96f887915df8bd61569590815b3e
Nikola07 <paul-jfg@kabelmail.de>
2018-01-25 14:52:26 +0100

    implemented cleaner

--- src/CorpusCleaner.py
+++ src/CorpusCleaner.py
@@ -1,5 +1,8 @@
 class ArgumentVector(object):
 
     def cleanCorpus(corpus):
+        for article in corpus:
+            if article.content is None:
+                corpus.remove(article)
 
         return corpus
