e123157efe0d2bfdfc35299651a61aeea08af47c
Paul Nikolaus <nikola07@gw.uni-passau.de>
2018-03-13 11:33:30 +0100

    removed unused method

--- src/main/ArticleReader.py
+++ src/main/ArticleReader.py
@@ -37,11 +37,9 @@ class ArticleReader():
             tree = ET.parse(fileid, parser=parser)
             for elem in tree.iter(tag='artikel'):
                 add_article = Article()
-
                 for child in elem.iter(tag='metadaten'):
                     for id in child.iter(tag='artikel-id'):
                         add_article.id = id.text
-
                 for child in elem.iter(tag='inhalt'):
                     if child.tag is None:
                         break
@@ -50,7 +48,7 @@ class ArticleReader():
                             break
                         for titel_liste in baby.iter(tag='titel-liste'):
                             for title in titel_liste.iter(tag='titel'):
-                                add_article.title = title
+                                add_article.title = title.text
                         article_text = ""
                         for text in baby.iter(tag='absatz'):
                             if text.text is None:
@@ -71,3 +69,55 @@ class ArticleReader():
         # print(len(politic))
         return articles
         # return politik
+
+    def read_articles_learn(loc_corpus, article_length):
+
+        texts_online = glob(loc_corpus + '/*')
+        texts_magazine = glob(loc_corpus + '/*')
+        years_online = []
+        years_magazine = []
+        for text in texts_online:
+            if text.endswith('.xml'):
+                years_online += glob(text)
+        for text in texts_magazine:
+            if text.endswith('.xml'):
+                years_magazine += glob(text)
+
+        reader_online = XMLCorpusReader(loc_corpus, years_online)
+        reader_magazine = XMLCorpusReader(loc_corpus, years_magazine)
+        # reader_corpus = PlaintextCorpusReader(loc_corpus + '/Magazin/Corpus-Magazin', '.*', encoding='utf-16')
+
+        articles = []
+        """
+        for fileid in reader_online.fileids():
+            words = reader_online.words(fileid)
+            string = ' '.join(words)
+            article_list = string.split('PMGSPON')
+            for article in article_list:
+                if (len(article.split(' ')) > article_length):
+                    add_article = Article()
+                    add_article.content = article
+                    articles.append(add_article)
+            print(len(article_list))
+        """
+        # an example for the magazine corpus
+        for fileid in reader_magazine.fileids():
+            words = reader_magazine.words(fileid)
+            string = ' '.join(words)
+            article_list = string.split('SP DER SPIEGEL')
+            for article in article_list:
+                if (len(article.split(' ')) > article_length):
+                    add_article = Article()
+                    add_article.content = article
+                    articles.append(add_article)
+            print(len(article_list))
+        """
+        for fileid in reader_corpus.fileids():
+            if len(reader_corpus.raw(fileid)) > article_length:
+                articles.append(reader_corpus.raw(fileid))
+                print(len(reader_corpus.raw(fileid)))
+        """
+        politic = [article for article in articles if article.content.__contains__('Politik')]
+        print(len(articles))
+        print(len(politic))
+        return politic
